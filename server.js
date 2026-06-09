6425bed66ffc4f0cc1a70107461ffc79eae7e7c5
Muthurajan P. <1404@smartcliff.loc>
2026-06-09 22:09:21 +0530

    commit

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use('/Developers Backup/LMS', express.static('\\\\192.168.1.4\\Developers Ba
 // Init Middleware
 app.use(express.json({ extended: false }));
 app.use(cors({
-  origin: ["https://lms-smartcliff-88rfz6r0m-muthurajanparthsarathys-projects.vercel.app", "http://localhost:3001", "http://localhost:3002"],
+  origin: ["https://lms-smartcliff-f0i3me1ac-muthurajanparthsarathys-projects.vercel.app", "http://localhost:3001", "http://localhost:3002"],
   methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
   credentials: true,
   exposedHeaders: ["Content-Length", "Authorization"],
